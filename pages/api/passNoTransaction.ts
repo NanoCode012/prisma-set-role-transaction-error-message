a6e2f6e30be201dfaa7f6ea5fa1062f48c02d2f0
NanoCode012 <kevinvong@rocketmail.com>
2022-08-29 22:02:00 +0700

    Add test without transaction

---
+++ pages/api/passNoTransaction.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let prisma;
+  try {
+    prisma = new PrismaClient();
+    console.log("Clearing locations table");
+    await prisma.location.deleteMany();
+
+    console.log("First time running");
+    await prisma.location.create({
+      data: {
+        name: "A",
+        abbr: "a",
+      },
+    });
+
+    console.log("Second time running");
+    await prisma.location.create({
+      data: {
+        name: "A",
+        abbr: "b",
+      },
+    });
+
+    await prisma.$disconnect();
+    res.status(200).json({ status: "success" });
+  } catch (e) {
+    if (prisma) await prisma.$disconnect();
+    console.error(e);
+    res.status(404).json({ status: "failed" });
+  }
+}
